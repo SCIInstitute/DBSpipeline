193319d2cd8b5a32fa62b1bc39505050940113d5
Jess Tate <jess@sci.utah.edu>
2024-03-20 15:01:40 -0600

    scripts for setting up envs. but it doesn't really work

--- scripts/makeSysConfig.sh
+++ scripts/makeSysConfig.sh
@@ -1,37 +1,94 @@
 #!/bin/bash
 
+# set up paths to run the DBSPipeline code.
+# https://github.com/SCIInstitute/DBSpipeline
 
-hname=$(hostname)
+ . ./sysUtils.sh
 
-sysname=${hname%%.*}
-#echo $hname
-echo System: $sysname
+# custom functions
+sysname=$(getSysName)
+echo System: $(getSysName)
 
-script=$(readlink -f $0)
-cwd=$(dirname $script)
-codedir=${cwd%/*}
+codedir=$(getCodeDir)
+echo using $(getCodeDir) as code directory
 
-echo using $codedir as code directory
-
-configdir=$codedir/configs
-mkdir -p $configdir
+configdir=$(getConfigDir)
 
 sysconfig_fname=$configdir/$sysname.config
 
-echo $sysconfig_fname
 
-message="enter data root location.  This is usually Dropbox folder: "
-d_dir=""
-while [ ! -d "$d_dir" ]
-do
-read -p "$message" d_dir
-done
+if ! $(checkSysConfig "$sysconfig_fname")
+then
+  echo config file not found.  Making new one:
+  echo $sysconfig_fname
+else
+#  echo $sysconfig_fname
+  read -p "config file $sysconfig_fname exists.
+  Overwrite [y/N]" owrite
+  if [ ! "$owrite" == "y" ] || [ -z "${owrite}" ]
+  then
+    echo Exiting
+    exit
+  fi
+fi
 
-data_dir=$(readlink -f $d_dir)
+
+message="enter data root location.  This is usually Dropbox folder:
+"
+
+data_dir=$(getUserDir "$message" | tail -n 1 )
+
+echo ---- testing:
+echo $data_dir
+
 
 echo Data dir is: $data_dir
 
 # tmp directory
 # tracking directories
 # envs
+
+#/Users/jess/Dropbox/CT DBS Human/CENTURY S Patients/pDummy_connectome
+
+# default temp in unix - /var/tmp/
+
+tempdirroot=/var/tmp
+tempdir=$tempdirroot/DBSPipeline
+#echo making a temp directory
+
+temp_message="
+default temp location is:
+$tempdirroot
+We will make a directory (DBSPipeline) here to use.
+do you want to set a different location? [y/N]:"
+read -p "$temp_message" tmp_q
+
+if [ "$tmp_q" == "y" ]
+then
+  message="enter Temp directory path:
+"
+  tempdirroot=$(getUserDir "$message" | tail -n 1 )
+fi
+
+tempdir=$tempdirroot/DBSPipeline
+echo  temp dir is: $tempdir
+mkdir -p $tempdir
+
+echo writing config file
+echo $sysconfig_fname
+
+header_str="##############
+# system config file for the DBS pipeline code.
+# config files should not be added to github,
+# except for public/shared systems and paths
+#
+
+
+"
+
+echo "$header_str" > $sysconfig_fname
+echo SYSNAME=\"$sysname\" >> $sysconfig_fname
+echo DATADIR=\"$data_dir\" >> $sysconfig_fname
+echo CODEDIR=\"$codedir\" >> $sysconfig_fname
+echo TEMPDIR=\"$tempdir\" >> $sysconfig_fname
 
---
+++ scripts/sysUtils.sh
@@ -0,0 +1,99 @@
+#/bib/bash
+
+# set up paths to run the DBSPipeline code.
+# https://github.com/SCIInstitute/DBSpipeline
+
+checkSysConfig(){
+  local sysconfig_fname="$1"
+#  echo $sysconfig_fname
+  if [ ! -f "$sysconfig_fname" ]; then
+#    echo file does not exist
+    exit 0
+  fi
+  
+  echo true
+  exit 1
+  
+}
+
+getSysName() {
+  hname=$(hostname)
+  sysname=${hname%%.*}
+  echo $sysname
+}
+
+getCodeDir() {
+  script=$(readlink -f $0)
+  cwd=$(dirname $script)
+  codedir=${cwd%/*}
+  echo $codedir
+}
+
+getConfigDir () {
+  configdir=$(getCodeDir)/config_files
+  mkdir -p $configdir
+  echo $configdir
+}
+
+getUserDir () {
+# trying local scope because of rewriting
+  if [ -z "$1" ]
+  then
+    echo Line message needed
+    exit 0
+  fi
+
+  local message="$1"
+  local d_dir=" "
+
+  if [ ! -z $2 ]
+  then
+    local d_dir="$2"
+  fi
+
+  echo $d_dir
+
+  while [ ! -d "$d_dir" ]
+  do
+    echo not found
+    read -p "$message" d_dir
+  done
+
+  local data_dir=$(readlink -f "$d_dir")
+  echo $data_dir
+
+}
+
+
+readConfigFile () {
+local fname="$1"
+
+while read varline
+do
+  if [[ "$varline" == "#"* ]] || [ -z "$varline" ]; then continue; fi
+  
+  eval "export $varline"
+done <$fname
+
+echo $SYSNAME
+export $SYSNAME
+
+}
+
+
+main() {
+  echo "runnin utils"
+}
+
+if [ "${BASH_SOURCE[0]}" = "$0" ]; then
+    # This script is being run.
+    __name__="__main__"
+else
+    # This script is being sourced.
+    __name__="__source__"
+fi
+
+
+if [ "$__name__" = "__main__" ]; then
+    main "$@"
+fi
