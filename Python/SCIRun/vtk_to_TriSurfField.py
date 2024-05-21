8d4398ac5d43f03f54b1a2764a6769c7d0c5163e
jessdtate@gmail.com <jess@sci.utah.edu>
2024-05-21 00:32:09 -0600

    some path debuging

--- Python/SCIRun/vtk_to_TriSurfField.py
+++ Python/SCIRun/vtk_to_TriSurfField.py
@@ -5,6 +5,8 @@
 # here is the current workaround
 import sys,os
 sys.path.insert(1, os.path.realpath(os.path.pardir))
+print(os.path.realpath(os.path.pardir))
+
 from utils.meshfiles import vtk_to_TriSurfField
 
 subject = sys.argv[1]
