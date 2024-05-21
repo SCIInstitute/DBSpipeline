7b2e4bfae747ba8a53f5b30a6fb5301f751ba193
jessdtate@gmail.com <jess@sci.utah.edu>
2024-05-20 20:44:44 -0600

    some path cleanup.  Nrrd converter missing

--- Python/utils/ConnectomeMover.py
+++ Python/utils/ConnectomeMover.py
@@ -8,6 +8,7 @@ Created on Tue Apr 30 11:09:29 2024
 import os
 import subprocess
 import shutil
+import argparse
 import pandas as pd
 
 parser = argparse.ArgumentParser(description='Inputs')
@@ -18,11 +19,17 @@ parser.add_argument('--subject',action='store',dest='subject',default=0)
 #parser.add_argument('--matrix',action='store',dest='c_matrix',default=0)
 args = parser.parse_args()
 
-rel_path1 = "Connectome"
-rel_path2 = "Tractography"
+if os.environ["SYSNAME"]=="hipergator":
+  rel_path1 = "Connectome"
+  rel_path2 = "Tractography"
+  rel_path3 = "SCIRun" # not relavent on hipergator yet?
+else:
+  rel_path1 = "MRTrix/Connectome"
+  rel_path2 = "MRTrix/Tractography"
+  rel_path3 = "MRtrix/SCIRun"
 
 connectomeMakerDir = os.path.join(os.environ["DATADIR"],  args.subject, rel_path1)
-stimVolumeDir = os.path.join(os.environ["DATADIR"],  args.subject, "SCIRun_MRtrix/Stim_volumes")
+stimVolumeDir = os.path.join(os.environ["DATADIR"],  args.subject, rel_path3, "Stim_volumes")
 #connectomeMakerDir = r"Z:\Dropbox (UFL)\CT DBS Human\CENTURY S Patients\p102 (44)\MRtrix\Connectome\Stim"
 #stimVolumeDir = r"Z:\Dropbox (UFL)\CT DBS Human\CENTURY S Patients\p102 (44)\MRtrix\SCIRun\Stim_volumes"
 
@@ -36,8 +43,14 @@ for i in BilateralStim.index:
     for side in ["Left", "Right"]:
         # Convert NRRD first here.
         #print("python3 /mnt/d/Github/DBSpipeline/Python/MRtrix/NRRDConverter.py --NRRD " + stimVolumeDir + os.path.sep + BilateralStim[side][i])
-        subprocess.call('python3 ' + os.path.join(os.environ["CODEDIR"], "Python/MRtrix/NRRDConverter.py") + ' --NRRD "' + stimVolumeDir + os.path.sep + BilateralStim[side][i] + '"', shell=True)
-        shutil.copy(os.path.join(stimVolumeDir, BilateralStim[side][i].replace(".nrrd",".nii.gz")), os.path.join(connectomeMakerDir, "temp_" + side + ".nii.gz"))
+        call_string='python3 ' + os.path.join(os.environ["CODEDIR"], "Python/MRtrix/NRRDConverter.py") + ' --NRRD "' + os.path.join(stimVolumeDir, BilateralStim[side][i]) + '"'
+        print(call_string)
+        subprocess.call(call_string, shell=True)
+        file_1 = os.path.join(stimVolumeDir, BilateralStim[side][i].replace(".nrrd",".nii.gz"))
+        file_2 = os.path.join(connectomeMakerDir, "temp_" + side + ".nii.gz")
+        print(file_1)
+        print(file_2)
+        shutil.copy(file_1, file_2)
     
     # run HCP_to_subject
     
