fcb71791c77895f7eb0c84dd35994e22869da6f3
Matthew Hook <mphook@ufl.edu>
2024-08-02 14:18:54 -0400

    Update Processing Scripts

--- Bash/ANTs/nonlin_warp.sh
+++ Bash/ANTs/nonlin_warp.sh
@@ -17,4 +17,5 @@ img_moving=$2
 
 antsRegistration -o nonlin_warp_ -d 3 --float --use-histogram-matching -r [$img_fixed,$img_moving,1] -t Affine[0.01] -m MI[$img_fixed,$img_moving,1,100,Random,0.02] -c [500x500x50,1e-8,20] -f 3x2x1 -s 4x2x1vox -t SyN[0.25] -m CC[$img_fixed,$img_moving,1,4] -c [50x50x50,0,5] -f 4x2x1 -s 1x0.5x0vox -v 1
 
+antsApplyTransforms -d 3 -i AAN_Brainstem_MNI152_1mm_v2p0.nii -r $img_moving -o AAN.nii.gz -n GenericLabel -t [nonlin_warp_0GenericAffine.mat,1] -t nonlin_warp_1InverseWarp.nii.gz
 date
--- Bash/MRtrix/Whole_brain_tract.sh
+++ Bash/MRtrix/Whole_brain_tract.sh
@@ -160,7 +160,7 @@ fi
 
 sr_out_tck_100K=${sub_dir}/SCIRun_files/whole_brain_100k
 py_call="python $CODEDIR/Python/MRtrix/tckConverter.py ${out_tck_100k_ACPC} ${sr_out_tck_100K}"
-a
+
 if [ "$dryrun" = false ]; then
   eval "$py_call"
 else
