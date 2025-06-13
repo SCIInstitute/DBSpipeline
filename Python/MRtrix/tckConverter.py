f83f94fc976d0cc526cbe865e14b433b28716896
jessdtate@gmail.com <jess@sci.utah.edu>
2025-06-13 12:57:46 -0600

    revert some changes

--- Python/MRtrix/tckConverter.py
+++ Python/MRtrix/tckConverter.py
@@ -31,13 +31,13 @@ def convertPtsEdges(filename, tract_datafile=None):
     for track in extractedTckFile.streamlines:
         trackNodes = track.copy()
         pts[currentPts:currentPts+trackNodes.shape[0],:] = trackNodes * [-1,-1,1]
-#        track_index[currentPts:currentPts+trackNodes.shape[0]] = k
+        track_index[currentPts:currentPts+trackNodes.shape[0]] = k
         edges[currentEdges:currentEdges+trackNodes.shape[0]-1,0] = np.arange(currentPts,currentPts+trackNodes.shape[0]-1, dtype=int)
         edges[currentEdges:currentEdges+trackNodes.shape[0]-1,1] = np.arange(currentPts+1,currentPts+trackNodes.shape[0], dtype=int)
 
         if tract_datafile:
-#            tract_data[currentPts:currentPts+trackNodes.shape[0]] = original_data[counter]
-            tract_data[currentPts:currentPts+trackNodes.shape[0]] = counter
+            tract_data[currentPts:currentPts+trackNodes.shape[0]] = original_data[counter]
+#            tract_data[currentPts:currentPts+trackNodes.shape[0]] = counter
             counter += 1
         
         currentPts += trackNodes.shape[0]
