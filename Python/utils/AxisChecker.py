fafaf852fc5f77443ae062cbc42b5a70726ec1b1
Jess Tate <jessdtate@gmail.com>
2025-12-23 02:42:25 -0700

    some more useful axis comparisons

--- Python/utils/AxisChecker.py
+++ Python/utils/AxisChecker.py
@@ -7,7 +7,7 @@ import nrrd
 import numpy as np
 import nibabel as nib
 
-from AxisCheckerDefaults import axisObjectTemplate, nrrdSpaceCodes, chiralityTable
+from AxisCheckerDefaults import axisObjectTemplate, nrrdSpaceCodes, chiralityTable, codeAxes, axisLookupTable
 
 
 def build_parser():
@@ -166,60 +166,187 @@ def checkNiftiAxes(filename, **kwargs):
 
   return getNiftiAxes(filename, **kwargs)
   
+def checkChiral(rot_mat, **kwargs):
+# assumes normalized matrix
+
+  precision_check = 0.1
+  
+  chir = None
+  ax0 = rot_mat[:,0]
+  ax1 = rot_mat[:,1]
+  ax2 = rot_mat[:,2]
+  
+#  print("checking chirality of matrix = ")
+#  print(rot_mat)
+#  print("ax0 = ", ax0)
+#  print("ax1 = ", ax1)
+#  print("ax2 = ", ax2)
+  
+  ax2_check = np.cross(ax0, ax1)
+  ax_check = np.dot(ax2, ax2_check)
+  
+#  print("ax2 check = ", ax2_check)
+#  print("dot check = ", ax_check)
+  
+  if np.abs(np.abs(ax_check)-1) > precision_check:
+    print("matrix might not be orthogonal")
+    return
+  
+  if ax_check>0:
+    chir = "right"
+  else:
+    chir = "left"
+    
+  return chir
+  
+
+def checkRotationAngle(rot1, rot2, **keargs):
+# assumes normalized matrix
+#  print(rot1)
+#  print(rot2)
+
+  diff_mat = np.dot(rot1, rot2.T)
+#  print("different mat : ", diff_mat)
+  
+  cos_theta =  (np.trace(diff_mat)-1)/2
+#  print("cos_theta = ", cos_theta)
+  
+  cos_theta_safe = np.max((np.min((cos_theta,1.0)), -1.0))
+  
+  theta = np.arccos(cos_theta_safe)
+#  print("theta = ", theta)
+  
+  return theta, diff_mat
+  
+def matrix2Code(rot_mat, **kwargs):
+# assumes a basis of RAS
+  
+  ind_arr = np.empty(3)
+  neg_arr = np.zeros(3)
+  for k in range(3):
+    ax = rot_mat[:,k]
+    mx_val = np.dot(ax, np.eye(3))
+#    print(mx_val)
+    ind = np.argmax(np.abs(mx_val))
+    ind_arr[k] = ind
+    if mx_val[ind]<0:
+      neg_arr[k] = 1
+      
+  code_arr = [ axisLookupTable[ng][id] for id, ng in zip(ind_arr.astype(int), neg_arr.astype(int))]
+  
+#  print(code_arr)
+      
+ 
+  return "".join(code_arr)
+  
+def normalizeRotation(rot_mat, **kwargs):
+  norm = np.sqrt(np.sum(rot_mat*rot_mat, axis = 0))
+  rot = rot_mat/norm
+  
+  return rot
+
+  
 def checkRotations(axes1, axes2, **kwargs):
   dkwargs = {"max_theta" : np.pi/6 }
   kwargs = {**dkwargs, **kwargs}
-  
-  #TODO: check for permutation differences?
   
   chir1 = next(key for key, v_list in chiralityTable.items() if  axes1["axis_space"] in v_list )
   chir2 = next(key for key, v_list in chiralityTable.items() if  axes2["axis_space"] in v_list )
   
   print(chir1, chir2)
 
-  norm1 = np.sqrt(np.sum(axes1["affine"][0:3,0:3]*axes1["affine"][0:3,0:3], axis = 0))
-  rot1 = axes1["affine"][0:3,0:3]/norm1
   
-  print(axes1["affine"][0:3,0:3])
+  rot1 = normalizeRotation(axes1["affine"][:3,:3])
+  
+  print("check axis 1")
+  print(axes1["affine"][:3,:3])
   print(rot1)
-  print(norm1)
-  
-  norm2 = np.sqrt(np.sum(axes2["affine"][0:3,0:3]*axes2["affine"][0:3,0:3], axis = 0))
-  rot2 = axes2["affine"][0:3,0:3]/norm2
-  
-  print(axes2["affine"][0:3,0:3])
+  mat_code1 = matrix2Code(rot1)
+  print("estimated axes : ", mat_code1)
+
+  rot2 = normalizeRotation(axes2["affine"][:3,:3])
+
+  print("check axis 2")
+  print(axes2["affine"][:3,:3])
   print(rot2)
-  print(norm2)
+  mat_code2 = matrix2Code(rot2)
+  print("estimated axes : ", mat_code2)
+
+  mat_chir1 = checkChiral(rot1)
+  mat_chir2 = checkChiral(rot2)
+  print("matrix chirality : ", mat_chir1, mat_chir2)
   
-  diff_mat = np.dot(rot1, rot2.T)
+  if not mat_chir1 == mat_chir2:
+    print("axes are not the same chirality")
+    
+  
+  theta, diff_mat = checkRotationAngle(rot1, rot2)
+  
   print("different mat : ", diff_mat)
-  
-  cos_theta =  (np.trace(diff_mat)-1)/2
-  print("cos_theta = ", cos_theta)
-  
-  theta = np.arccos(cos_theta)
   print("theta = ", theta)
   
+#  rot2_check = np.dot(diff_mat, rot2)
+#  
+#  theta_check, diff_mat_check = checkRotationAngle(rot1, rot2_check)
   
-  perm_mat = np.round(diff_mat)
-  print("permutation mat : ", perm_mat)
+#  print("different mat check: ", diff_mat_check)
+#  print("theta check = ", theta_check)
+
+
+
+  perm = False
+
+  ax_mat1 = np.array([ codeAxes[ax] for ax in axes1["axis_space"]]).T
+#  print(ax_mat1)
+  ax_mat2 = np.array([ codeAxes[ax] for ax in axes2["axis_space"]]).T
+#  print(ax_mat2)
+
+  if chir1 == mat_chir1 or chir1 == "RAS":
+    rot1_ = rot1
+  else:
+    print("permuted matrix 1 = ")
+    rot1_ = np.dot(ax_mat1,rot1)
+    print(rot1_)
+    perm = True
   
-  rot2_ = np.dot(perm_mat.T, rot2)
-  print(rot2_)
+#  perm_mat = np.dot(ax_mat1, ax_mat2.T)
+#  print(perm_mat)
+
+  if chir2 == mat_chir2 or chir2 == "RAS":
+    rot2_ = rot2
+  else:
+    print("permuted matrix 2 = ")
+    rot2_ = np.dot(ax_mat2,rot2)
+    print(rot2_)
+    perm = True
+
   
-  diff_mat_ = np.dot(rot1, rot2_)
-  print("different mat perm : ", diff_mat_)
-  
-  cos_theta_ =  (np.trace(diff_mat_)-1)/2
-  print("perm cos_theta = ", cos_theta_)
-  
-  theta_ = np.arccos(cos_theta_)
-  print("perm theta = ", theta_)
-  
-  if theta>kwargs["max_theta"]:
-    if theta_<kwargs["max_theta"]:
-      print("potential match with permutations")
-      # TODO: probably expand on this case set?
+  if perm:
+    
+    print("perm mat 1 = ")
+    print(rot1_)
+    print("perm mat 2 = ")
+    print(rot2_)
+    
+    theta_, diff_mat_ = checkRotationAngle(rot1_, rot2_)
+    print("different mat perm : ", diff_mat_)
+    print("perm theta = ", theta_)
+    
+    
+#    rot2_check_ = np.dot(diff_mat_, rot2_)
+#    
+#    print("mat 2 perm check= ")
+#    print(rot2_check_)
+#    
+#    theta_check_, diff_mat_check_ = checkRotationAngle(rot1_, rot2_check_)
+#    
+#    print("different mat perm check: ", diff_mat_check_)
+#    print("perm theta check = ", theta_check_)
+    
+    if theta>kwargs["max_theta"]:
+      if theta_<kwargs["max_theta"]:
+        print("--- potential match with different axis interpretations ---")
+        # TODO: probably expand on this case set?
   
   return theta<kwargs["max_theta"]
   
--- Python/utils/AxisCheckerDefaults.py
+++ Python/utils/AxisCheckerDefaults.py
@@ -12,15 +12,50 @@ axisObjectTemplate = {
         "header" : None
 }
 
-# space dims of 3 only
+# space dims of 3 only.
+# The assumption is that RAS is the base system,
+#   and is therefore the same as scanner-xyz
 nrrdSpaceCodes = {
     "RAS" : ["right-anterior-superior",  "RAS"],
     "LAS" : ["left-anterior-superior", "LAS"],
     "LPS" : ["left-posterior-superior", "LPS"],
-    "XYZ" : ["scanner-xyz"],
-    "RH"  : ["3D-right-handed"],
-    "LH"  : ["3D-left-handed"]
+    "RAS" : ["scanner-xyz"],
+    "RAS"  : ["3D-right-handed"],
+    "LAS"  : ["3D-left-handed"]
+#    "XYZ" : ["scanner-xyz"],
+#    "RH"  : ["3D-right-handed"],
+#    "LH"  : ["3D-left-handed"]
 }
+
+
+
+#compared to RAS = XYZ = RH
+#codeAxes = {
+#    "RAS" : np.eye(3),
+#    "LAS" : np.array([[-1, 0, 0], [ 0, 1, 0], [0, 0, 1]]),
+#    "LPS" : np.array([[-1, 0, 0], [0, -1, 0], [0, 0, 1]]),
+#    "LIA" : np.array([[-1, 0, 0], [0, 0, 1], [0, -1, 0]]),
+#    "ALS" : np.array([[0, -1, 0], [1, 0, 0], [0, 0, 1]]),
+#    "PRS" : np.array([[0, 1, 0], [-1, 0, 0], [0, 0, 1]]),
+#    "RIP" : np.array([[-1, 0, 0], [0, -1, 0], [0, 0, 1]]),
+#    "XYZ" : np.eye(3),
+#    "RH"  : np.eye(3),
+#    "LH"  : np.array([[-1, 0, 0], [ 0, 1, 0], [0, 0, 1]])
+#}
+
+codeAxes = {
+    "R" : np.array([1,0,0]),
+    "A" : np.array([0,1,0]),
+    "S" : np.array([0,0,1]),
+    "L" : np.array([-1,0,0]),
+    "P" : np.array([0,-1,0]),
+    "I" : np.array([0,0,-1])
+}
+
+axisLookupTable = [["R", "A", "S"], ["L", "P", "I"]] 
+
+
+
 
 # do I need to make these exhaustive?
 chiralityTable = {
