e9a35b41a39753d4ef361b0db25c736ae29e641d
Jess Tate <jessdtate@gmail.com>
2026-04-16 00:20:24 -0600

    I think I got the basic functionality working

--- Python/Dataflow/makeProfile.py
+++ Python/Dataflow/makeProfile.py
@@ -1,8 +1,13 @@
 # prototype profile for connectomics from the CL
 import json
 import numpy as np
+import argparse
 
 import os
+
+from defaultProfile import experiment_required_fields, def_Makeprofile, files_ignore, def_print_contents, base_profile_keys, def_baseProfile, def_implantation
+
+
 
 scriptpath = os.path.dirname(os.path.abspath(__file__))
 #print(scriptpath)
@@ -40,7 +45,7 @@ def build_parser():
 
   # This will be implemented as rollout broadens
   parser.add_argument("-d", "--datapath", required=False,
-                      help="path to were the data are located. If not provided, the script will try to use the enviroment variable DATADIR", default
+                      help="path to were the data are located. If not provided, the script will try to use the enviroment variable DATADIR",
                       dest="datapath")
   parser.add_argument("-p", "--profilepath", required=False,
                       help="path to profile directory. default location is <datapath>/profiles",
@@ -61,13 +66,13 @@ def check_parser(args):
     
       
   if use_environ:
-    if os.environ["DATADIR"]
+    if os.environ["DATADIR"]:
       args.datapath = os.environ["DATADIR"]
     else:
       raise ValueError("environment variable DATADIR not set.  use -d flag to provide path or set DATADIR")
   
   use_defaultpath = True
-  profilepath =
+  profilepath = ""
   if args.profilepath:
     if os.path.exists(args.profilepath):
       use_defaultpath = False
@@ -75,8 +80,8 @@ def check_parser(args):
       print("the provided path, "+args.profilepath+"does not exist.  Using default path")
   
   if use_defaultpath:
-    args.profilepath = os.path.join(datapath, "profiles")
-    if os.path.exists(args.profilepath):
+    args.profilepath = os.path.join(args.datapath, "profiles")
+    if not os.path.exists(args.profilepath):
       os.makedirs(args.profilepath)
   
   experimentfile = args.experimentfile
@@ -85,29 +90,30 @@ def check_parser(args):
     if not os.path.exists(args.experimentfile):
       args.experimentfile = os.path.join(args.profilepath, args.experimentfile)
     else:
-      raise ValueError("cannot file experiment file "+experimentfile+" or "+ args.experimentfile)
+      raise ValueError("cannot find experiment file "+experimentfile+" or "+ args.experimentfile)
       
   return args
   
 def readExperimentFile(args):
-  with open(experimentfile, 'r') as json_file:
+  print(args.experimentfile)
+  with open(args.experimentfile, 'r') as json_file:
     experiment = json.load(json_file)
-    
+  
+#  print(experiment)
+  
   check_experiment(experiment, args)
   
   return experiment
   
 def check_experiment(experiment, args):
   
-  required_fields = ["experiment_name", "subjects", "lookup_table", "left_ROI", "right_ROI"]
+  for key in experiment_required_fields:
+    if not key in experiment.keys():
+      raise ValueError("Required experiment inputs:", ", ".join(experiment_required_fields))
   
-  for key in required_fields:
-    if not key in experiment:
-      raise ValueError("Required experiment inputs:", ", ".join(required_fields))
-  
-  print(print_folder(args.datapath))
+#  print(print_folder(args.datapath))
       
-  for sub in subjects:
+  for sub in experiment["subjects"]:
     sub_dir = os.path.join(args.datapath, sub)
     missing = []
     if not os.path.exists(sub_dir):
@@ -115,9 +121,10 @@ def check_experiment(experiment, args):
     else:
       #TODO: check file structure
       subfolders = get_contents(sub_dir)
-      
-   if len(missing)>0:
-      raise ValueError("One or more subjects provided in experiment profile are missing")
+#      print(print_contents(subfolders))
+#      print(print_folder(sub_dir))
+  if len(missing)>0:
+    raise ValueError("One or more subjects provided in experiment profile are missing")
     
     
   return True
@@ -135,7 +142,8 @@ def get_contents(directory):
   files={}
   directories = {}
   for child in children:
-    if child[0]==".":
+    if child[0]=="." or child in files_ignore:
+#      print("skip ", child)
       continue
     child_path = os.path.join(directory, child)
     if os.path.isdir(child_path):
@@ -146,22 +154,103 @@ def get_contents(directory):
           "size" : os.path.getsize(child_path),
           "time" : os.path.getmtime(child_path)
       }
-    contents = {"files" : files, "directories" :  directories}
+  contents = {"files" : files, "directories" :  directories}
+#  print(contents)
 #  output = {"path" : parent, folder : contents}
   return contents
   
-def print_folder(directory):
+def print_folder(directory, **kwargs):
   parent, folder = os.path.split(directory)
   
   contents = get_contents(directory)
-  
-  return string
+#  print(print_contents(contents, 1, 2, True))
+  return folder+"\n"+print_contents(contents, **kwargs)
 
-def print_contents(contents, indent = 2):
-  
+def print_contents(contents, **kwargs):
+  kwargs = {**def_print_contents, **kwargs}
+  indent = (" "*kwargs["indent_spaces"]+"| ")*kwargs["indent_level"]
+#  output_string = " "*(indent-1)+"|_ "
   output_string = ""
-  for
+  if not kwargs["dirs_only"]:
+    for file in contents["files"]:
+      output_string+=indent+file+"\n"
+  for dir_name, dir_content in contents["directories"].items():
+    output_string+=indent[:-1]+"-"+dir_name+"\n"
+    output_string+=print_contents(dir_content, **{**kwargs, "indent_level" : kwargs["indent_level"]+1} )
+    
+  return output_string
+#  print(output_string)
+#  for
+
+
+
+def getRootPath(profile, args):
+  return os.path.join(args.datapath, profile["subject"] )
+
+def getSegPath(profile, args):
+  return os.path.join(profile["rootPath"], "Segmentations")
   
+def getSRFilesPath(profile, args):
+  return os.path.join(profile["rootPath"], "SCIRun_files")
+  
+def getConnectomePath(profile, args):
+  return os.path.join(profile["rootPath"], "Connectome")
+  
+def getTractographyPath(profile, args):
+  return os.path.join(profile["rootPath"], "Tractography")
+
+def getCleanTractPath(profile, args):
+  return os.path.join(profile["tractographyPath"], "Cleaned")
+  
+def getFiberTractPath(profile, args):
+  return os.path.join(profile["cleantractPath"], "Fibers")
+  
+paths_table = {
+  "rootPath" : getRootPath,
+  "segPath" : getSegPath,
+  "SRFilesPath" : getSRFilesPath,
+  "connectomePath" : getConnectomePath,
+  "tractographyPath" : getTractographyPath,
+  "cleantractPath" : getCleanTractPath,
+  "fibertractPath" : getFiberTractPath,
+}
+
+
+def makeProfile(subject, args, **kwargs):
+  kwargs = {**def_Makeprofile, **kwargs}
+  
+  profile_filename = subject+"_"+kwargs["experiment"]+"_profile.json"
+  profile_file = os.path.join(args.profilepath, profile_filename)
+  
+  if os.path.exists(profile_file):
+    with open(profile_file, 'r') as json_file:
+      profile = json.load(json_file)
+  else:
+    profile = {}
+    
+  profile["subject"]=subject
+  profile["profile_file"] = profile_file
+  
+  for key, value in kwargs.items():
+    if key in base_profile_keys and value:
+      profile[key] = value #do I need to deep copy?
+  
+  for key in base_profile_keys:
+    if not key in profile.keys():
+      if key in def_baseProfile.keys():
+        profile[key] = def_baseProfile[key] #do I need to deep copy?
+      else:
+        # very order dependent right now
+        profile[key] = paths_table[key](profile,args)
+  
+  print(profile)
+  
+  
+  
+  
+  
+
+
 
 def main():
 
@@ -169,8 +258,15 @@ def main():
   args = parser.parse_args()
   args = check_parser(args)
   
-  experiment = readExperimentFile(arg.experimentfile)
+  experiment = readExperimentFile(args)
   
+  for sub in experiment["subjects"]:
+    makeProfile(sub, args,
+          experiment = experiment["experiment_name"],
+          lookup_table = experiment["lookup_table"],
+          left_ROI = experiment["left_ROI"],
+          right_ROI = experiment["right_ROI"]
+          )
   
     
 if __name__ == "__main__":
