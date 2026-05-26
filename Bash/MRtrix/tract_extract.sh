8cefd1e0ede623ceefbd74eb76e5e1dc58b6722f
Matthew Hook <mphook@ufl.edu>
2026-05-26 15:10:38 -0400

    Tract extraction overhaul

--- Bash/MRtrix/tract_extract.sh
+++ Bash/MRtrix/tract_extract.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=8
 #SBATCH --mem=8gb
 #SBATCH --time=1:00:00
 #SBATCH --job-name=tract_pull
@@ -16,6 +16,8 @@
 # sbatch --mail-user="user"@ufl.edu $CODEDIR/Bash/MRtrix/tract_extract.sh -r 361,366 -s p102 -p All -n Caudate
 # OR
 # sbatch --mail-user="user"@ufl.edu $CODEDIR/Bash/MRtrix/tract_extract.sh -s p102 -p All -n Caudate
+# OR
+# sbatch --mail-user="user"@ufl.edu $CODEDIR/Bash/MRtrix/tract_extract.sh -t MDPF -r 361,366 -s p102 -p All -n Caudate
 
 
 # TODO: Remove mesocircuit_pull.sh and incorporate exemplar data (exemplar_pull.sh)
@@ -32,13 +34,15 @@ Help()
 {
    # Display Help
    echo
-   echo "Syntax: scriptTemplate [-h|r|s|p|n]"
+   echo "Syntax: scriptTemplate [-h|r|s|p|n|t|c]"
    echo "options:"
    echo "-h    Help "
    echo "-r    Region(s) to project to. Can be a list. If not specified, all projections (per side) will be produced"
    echo "-s    Subject ID"
    echo "-p    Profile"
    echo "-n    Output Name"
+   echo "-t    Target Nuclei Name"
+   echo "-c    Contralateral Connections? (y)"
    echo
    echo "Example Mesocircuit Regions:"
    echo
@@ -57,19 +61,20 @@ Help()
 
 
 # Get the options
-while getopts ":h:r:s:p:n:" option; do
+while getopts ":h:r:s:p:n:t:c:" option; do
    case $option in
       r) regions=$OPTARG;;
       s) subject=$OPTARG;;
       p) profile=$OPTARG;;
       n) name=$OPTARG;;
+      t) targ=$OPTARG;;
+      c) cont=$OPTARG;;
       h | * | :) Help && exit;;
    esac
 done
 
 if [ $SYSNAME == "hipergator" ]
 then
-	module load mrtrix
   module load jq
 fi
 
@@ -81,19 +86,39 @@ mkdir -p ${output_dir}
 
 for side in left right
 do
-    mkdir -p ${output_dir}/tmp_${side}
-    lookup=$(jq -r -c .${side}_ROI[] ${subject}/${profile}profile.json) #JSON parsing
-    lookup_name=$(jq -r -c .lookup_table ${subject}/${profile}profile.json)
-    roi=$({ read; while IFS=, read -r col1 lookup_index MRtrix_index; do if (( $lookup_index == ${lookup} )); then echo ${MRtrix_index}; fi; done; } < ${sub_dir}/Connectome/${index_key}) #Matrix lookup value
-    label_name=$({ read; while IFS=, read -r Index Label File_index Filename Path; do if (( $Index == ${lookup} )); then echo ${Label}; fi; done; } < ${lookup_name}) #ROI name
+    if [ $SYSNAME == "hipergator" ]
+    then
+	    module load mrtrix
+    fi
+    side_big=${side^^}
+    side_marker=${side_big:0:1}
+    # mkdir -p ${output_dir}/tmp_${side}
+    lookup_name=$(jq -r -c .lookup_table ${sub_dir}/${profile}profile.json)
+    if [ -z $targ ]
+    then
+      echo -e "\n No target given, using the profile default\n"
+      lookup=$(jq -r -c .${side}_ROI[] ${sub_dir}/${profile}profile.json) #JSON parsing
+      roi=$({ read; while IFS=, read -r col1 lookup_index MRtrix_index; do if (( $lookup_index == ${lookup} )); then echo ${MRtrix_index}; fi; done; } < ${sub_dir}/Connectome/${index_key}) #Matrix lookup value
+      label_name=$({ read; while IFS=, read -r Index Label File_index Filename Path; do if (( $Index == ${lookup} )); then echo ${Label}; fi; done; } < ${lookup_name}) #ROI name
+    else
+      echo -e "\nUsing target as ROI"
+      label_name=${side_marker}_${targ}
+      echo $label_name
+      lookup=$({ read; while IFS=, read -r Index Label File_index Filename Path; do if [[ $Label == ${label_name} ]]; then echo ${Index}; fi; done; } < ${lookup_name})
+      roi=$({ read; while IFS=, read -r col1 lookup_index MRtrix_index; do if (( $lookup_index == ${lookup} )); then echo ${MRtrix_index}; fi; done; } < ${sub_dir}/Connectome/${index_key})
+      echo $roi
+      # exit 125
+    fi
     roi_name=${label_name##*_} #extract ROI name only, leave out side designation
     echo -e "\n${roi_name} ${side} to ${name}\n"
+    tmp_dir=${output_dir}/tmp_${roi_name}_${side}
+    mkdir -p $tmp_dir
     if [ -z $regions ]
     then
       nodes=${roi}
       echo -e "\nNo regions Specified, generating all connections\n"
       name=All
-      connectome2tck ${sub_dir}/Tractography/Cleaned/Fibers/whole_brain_fibers.tck ${sub_dir}/Tractography/Cleaned/${assignment} ${output_dir}/tmp_${side}/${name}_${nodes} -nodes $nodes -force -files single -tck_weights_in ${sub_dir}/Tractography/Cleaned/Fibers/sift2_weights.txt -prefix_tck_weights_out ${output_dir}/tmp_${side}/${name}_${nodes}
+      connectome2tck ${sub_dir}/Tractography/Cleaned/Fibers/whole_brain_fibers.tck ${sub_dir}/Tractography/Cleaned/${assignment} ${tmp_dir}/${name}_${nodes} -nodes $nodes -force -files single -tck_weights_in ${sub_dir}/Tractography/Cleaned/Fibers/sift2_weights.txt -prefix_tck_weights_out ${tmp_dir}/${name}_${nodes}
     else
       nodes=''
       for region in ${regions//,/ } #convert lookup index to MRtrix index
@@ -102,30 +127,49 @@ do
       done
       nodes=${nodes#*,},${roi}
       echo -e "\nGenerating region-specific connections\n"
-      connectome2tck ${sub_dir}/Tractography/Cleaned/Fibers/whole_brain_fibers.tck ${sub_dir}/Tractography/Cleaned/${assignment} ${output_dir}/tmp_${side}/${name}_ -nodes $nodes -exclusive -force -files per_node -tck_weights_in ${sub_dir}/Tractography/Cleaned/Fibers/sift2_weights.txt -prefix_tck_weights_out ${output_dir}/tmp_${side}/${name}_
+      connectome2tck ${sub_dir}/Tractography/Cleaned/Fibers/whole_brain_fibers.tck ${sub_dir}/Tractography/Cleaned/${assignment} ${tmp_dir}/${name}_ -nodes $nodes -exclusive -force -files per_node -tck_weights_in ${sub_dir}/Tractography/Cleaned/Fibers/sift2_weights.txt -prefix_tck_weights_out ${tmp_dir}/${name}_
     fi
     
    #  tckedit ${output_dir}/tmp_${side}/*${roi}* ${output_dir}/${roi_name}_${side}_${name}.tck -force
-    cp ${output_dir}/tmp_${side}/${name}_${roi}.csv ${output_dir}/${roi_name}_${side}_${name}.csv
-    tcktransform ${output_dir}/tmp_${side}/${name}_${roi}.tck ${sub_dir}/Tractography/Cleaned/transform.mif ${output_dir}/${roi_name}_${side}_${name}_ACPC.tck -force
-    if [ $side == "left" ]
+    cp ${tmp_dir}/${name}_${roi}.csv ${output_dir}/${roi_name}_${side}_${name}.csv
+    tcktransform ${tmp_dir}/${name}_${roi}.tck ${sub_dir}/Tractography/Cleaned/transform.mif ${output_dir}/${roi_name}_${side}_${name}_ACPC.tck -force
+    if [ -z $cont ]
     then
-      ex_zone=WM_right.nii.gz
+      echo -e "\nIpsilateral Connections\n"
+      if [ $side == "left" ]
+      then
+        ex_zone=WM_right.nii.gz
+      else
+        ex_zone=WM_left.nii.gz
+      fi
+      tckedit ${output_dir}/${roi_name}_${side}_${name}_ACPC.tck -exclude ${sub_dir}/Segmentations/White_matter/${ex_zone} ${output_dir}/${roi_name}_${side}_${name}_ACPC.tck -tck_weights_in ${output_dir}/${roi_name}_${side}_${name}.csv -tck_weights_out ${output_dir}/${roi_name}_${side}_${name}.csv -force
     else
-      ex_zone=WM_left.nii.gz
+      echo -e "\nContralateral Connections\n"
+      if [ $side == "left" ]
+      then
+        ex_zone=WM_right.nii.gz
+      else
+        ex_zone=WM_left.nii.gz
+      fi
+      tckedit ${output_dir}/${roi_name}_${side}_${name}_ACPC.tck -include ${sub_dir}/Segmentations/White_matter/${ex_zone} ${output_dir}/${roi_name}_${side}_${name}_ACPC.tck -tck_weights_in ${output_dir}/${roi_name}_${side}_${name}.csv -tck_weights_out ${output_dir}/${roi_name}_${side}_${name}.csv -force
     fi
-    tckedit ${output_dir}/${roi_name}_${side}_${name}_ACPC.tck -exclude ${sub_dir}/Segmentations/White_matter/${ex_zone} ${output_dir}/${roi_name}_${side}_${name}_ACPC.tck -tck_weights_in ${output_dir}/${roi_name}_${side}_${name}.csv -tck_weights_out ${output_dir}/${roi_name}_${side}_${name}.csv -force
     tckconvert ${output_dir}/${roi_name}_${side}_${name}_ACPC.tck ${output_dir}/${roi_name}_${side}_${name}_ACPC.vtk -force
-    rm -r ${output_dir}/tmp_${side}
+    rm -r ${tmp_dir}
+    if [ $SYSNAME == "hipergator" ]
+    then
+	    module load python
+    fi
+    python ${CODEDIR}/Python/MRtrix/tckConverter.py ${output_dir}/${roi_name}_${side}_${name}_ACPC.tck  ${output_dir}/${roi_name}_${side}_${name} --tract_data ${output_dir}/${roi_name}_${side}_${name}.csv
+    python ${CODEDIR}/Python/MRtrix/Add_Weights_To_VTK.py --input_vtk ${output_dir}/${roi_name}_${side}_${name}_ACPC.vtk --output_vtk ${output_dir}/${roi_name}_${side}_${name}_weights.vtk --tract_data ${output_dir}/${roi_name}_${side}_${name}.tckdata
 done
 
-if [ $SYSNAME == "hipergator" ]
-then
-	module load python
-fi
+# if [ $SYSNAME == "hipergator" ]
+# then
+# 	module load python
+# fi
 
-for side in left right
-do
-   python ${CODEDIR}/Python/MRtrix/tckConverter.py ${output_dir}/${roi_name}_${side}_${name}_ACPC.tck  ${output_dir}/${roi_name}_${side}_${name} --tract_data ${output_dir}/${roi_name}_${side}_${name}.csv
-   python ${CODEDIR}/Python/MRtrix/Add_Weights_To_VTK.py --input_vtk ${output_dir}/${roi_name}_${side}_${name}_ACPC.vtk --output_vtk ${output_dir}/${roi_name}_${side}_${name}_weights.vtk --tract_data ${output_dir}/${roi_name}_${side}_${name}.tckdata
-done
+# for side in left right
+# do
+#    python ${CODEDIR}/Python/MRtrix/tckConverter.py ${output_dir}/${roi_name}_${side}_${name}_ACPC.tck  ${output_dir}/${roi_name}_${side}_${name} --tract_data ${output_dir}/${roi_name}_${side}_${name}.csv
+#    python ${CODEDIR}/Python/MRtrix/Add_Weights_To_VTK.py --input_vtk ${output_dir}/${roi_name}_${side}_${name}_ACPC.vtk --output_vtk ${output_dir}/${roi_name}_${side}_${name}_weights.vtk --tract_data ${output_dir}/${roi_name}_${side}_${name}.tckdata
+# done
