e13ef9a878beec98c98eb927ec6f8997605e3d51
Matthew Hook <mphook@ufl.edu>
2026-07-20 12:59:05 -0400

    Upgraded connectome2tck file and error handling

--- Bash/MRtrix/tract_extract.sh
+++ Bash/MRtrix/tract_extract.sh
@@ -2,9 +2,9 @@
 
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=8
-#SBATCH --mem=8gb
-#SBATCH --time=1:00:00
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=4gb
+#SBATCH --time=00:30:00
 #SBATCH --job-name=tract_pull
 #SBATCH --mail-type=ALL
 #SBATCH --output=meso_%j.out
@@ -44,18 +44,18 @@ Help()
    echo "-t    Target Nuclei Name"
    echo "-c    Contralateral Connections? (y)"
    echo
-   echo "Example Mesocircuit Regions:"
+   echo "Pre-loaded Mesocircuit Regions for -r input, just give the (name), no need for -n either:"
    echo
-   echo "Anterior Cingulate and Medial Prefrontal Cortex: 58,238,59,239,57,237,180,360,61,241,60,240,179,359,62,242,64,244,165,345,63,243,69,249,65,245,88,268,164,344"
-   echo "Caudate: 361,366"
-   echo "DorsoLateral Prefrontal Cortex: 26,206,98,278,97,277,70,250,68,248,67,247,73,253,71,251,87,267,86,266,84,264,85,265,83,263"
-   echo "Inferior Parietal Cotrex: 143,323,146,326,145,325,144,324,148,328,116,296,147,327,149,329,150,330,151,331"
-   echo "Insular and Frontal Opercular Cortex: 103,283,178,358,168,348,167,347,106,286,115,295,110,290,112,292,109,289,114,294,108,288,169,349,111,291"
-   echo "Premotor Cortex: 96,276,54,234,10,190,11,191,12,192,56,236,78,258"
-   echo "Somatosensory and Motor Cortex: 8,188,9,189,53,233,51,231,52,232"
-   echo "Superior Parietal Cortex: 50,230,48,228,49,229,95,275,117,297,47,227,45,225,42,222,29,209,46,226"
-   echo "Temporo-Parieto-Occipital Junction: 140,320,141,321,139,319,28,208,25,205"
-   echo "Paracentral Lobular and Mid Cingulate Cortex: 39,219,36,216,37,217,40,220,41,221,55,235,44,224,43,223"
+   echo "(ACC) Anterior Cingulate and Medial Prefrontal Cortex: 58,238,59,239,57,237,180,360,61,241,60,240,179,359,62,242,64,244,165,345,63,243,69,249,65,245,88,268,164,344"
+   echo "(Caudate) Caudate: 361,366"
+   echo "(DLpfc) DorsoLateral Prefrontal Cortex: 26,206,98,278,97,277,70,250,68,248,67,247,73,253,71,251,87,267,86,266,84,264,85,265,83,263"
+   echo "(InfPar) Inferior Parietal Cotrex: 143,323,146,326,145,325,144,324,148,328,116,296,147,327,149,329,150,330,151,331"
+   echo "(InsFroOp) Insular and Frontal Opercular Cortex: 103,283,178,358,168,348,167,347,106,286,115,295,110,290,112,292,109,289,114,294,108,288,169,349,111,291"
+   echo "(Premotor) Premotor Cortex: 96,276,54,234,10,190,11,191,12,192,56,236,78,258"
+   echo "(SomaMot) Somatosensory and Motor Cortex: 8,188,9,189,53,233,51,231,52,232"
+   echo "(SupPar) Superior Parietal Cortex: 50,230,48,228,49,229,95,275,117,297,47,227,45,225,42,222,29,209,46,226"
+   echo "(TempParOcc) Temporo-Parieto-Occipital Junction: 140,320,141,321,139,319,28,208,25,205"
+   echo "(ParaLobMidCC) Paracentral Lobular and Mid Cingulate Cortex: 39,219,36,216,37,217,40,220,41,221,55,235,44,224,43,223"
    echo
 }
 
@@ -73,11 +73,39 @@ while getopts ":h:r:s:p:n:t:c:" option; do
    esac
 done
 
+
+# Pre-loaded regions
+case $regions in
+  ACC)
+    echo -e "\nACC region given\n"; name=$regions; regions=58,238,59,239,57,237,180,360,61,241,60,240,179,359,62,242,64,244,165,345,63,243,69,249,65,245,88,268,164,344;;
+  Caudate)
+    echo -e "\nCaudate region given\n"; name=$regions; regions=361,366;;
+  DLpfc)
+    echo -e "\nDLpfc region given\n"; name=$regions; regions=26,206,98,278,97,277,70,250,68,248,67,247,73,253,71,251,87,267,86,266,84,264,85,265,83,263;;
+  InfPar)
+    echo -e "\nInfPar region given\n"; name=$regions; regions=143,323,146,326,145,325,144,324,148,328,116,296,147,327,149,329,150,330,151,331;;
+  InsFroOp)
+    echo -e "\nInsFroOp region given\n"; name=$regions; regions=103,283,178,358,168,348,167,347,106,286,115,295,110,290,112,292,109,289,114,294,108,288,169,349,111,291;;
+  Premotor)
+    echo -e "\nPremotor region given\n"; name=$regions; regions=96,276,54,234,10,190,11,191,12,192,56,236,78,258;;
+  SomaMot)
+    echo -e "\nSomaMot region given\n"; name=$regions; regions=8,188,9,189,53,233,51,231,52,232;;
+  SupPar)
+    echo -e "\nSupPar region given\n"; name=$regions; regions=50,230,48,228,49,229,95,275,117,297,47,227,45,225,42,222,29,209,46,226;;
+  TempParOcc)
+    echo -e "\nTempParOcc region given\n"; name=$regions; regions=140,320,141,321,139,319,28,208,25,205;;
+  ParaLobMidCC)
+    echo -e "\nParaLobMidCC region given\n"; name=$regions; regions=39,219,36,216,37,217,40,220,41,221,55,235,44,224,43,223;;
+  *)
+    echo -e "\nNo pre-loads given, passing regions and name as is\n"
+esac
+
 if [ $SYSNAME == "hipergator" ]
 then
   module load jq
 fi
 
+echo $subject
 sub_dir="${DATADIR}/${subject}"
 output_dir=${sub_dir}/Tractography/Cleaned/Fibers/${name}
 assignment=assignments_${profile}.txt
@@ -92,7 +120,6 @@ do
     fi
     side_big=${side^^}
     side_marker=${side_big:0:1}
-    # mkdir -p ${output_dir}/tmp_${side}
     lookup_name=$(jq -r -c .lookup_table ${sub_dir}/${profile}profile.json)
     if [ -z $targ ]
     then
@@ -130,7 +157,6 @@ do
       connectome2tck ${sub_dir}/Tractography/Cleaned/Fibers/whole_brain_fibers.tck ${sub_dir}/Tractography/Cleaned/${assignment} ${tmp_dir}/${name}_ -nodes $nodes -exclusive -force -files per_node -tck_weights_in ${sub_dir}/Tractography/Cleaned/Fibers/sift2_weights.txt -prefix_tck_weights_out ${tmp_dir}/${name}_
     fi
     
-   #  tckedit ${output_dir}/tmp_${side}/*${roi}* ${output_dir}/${roi_name}_${side}_${name}.tck -force
     cp ${tmp_dir}/${name}_${roi}.csv ${output_dir}/${roi_name}_${side}_${name}.csv
     tcktransform ${tmp_dir}/${name}_${roi}.tck ${sub_dir}/Tractography/Cleaned/transform.mif ${output_dir}/${roi_name}_${side}_${name}_ACPC.tck -force
     if [ -z $cont ]
@@ -163,13 +189,3 @@ do
     python ${CODEDIR}/Python/MRtrix/Add_Weights_To_VTK.py --input_vtk ${output_dir}/${roi_name}_${side}_${name}_ACPC.vtk --output_vtk ${output_dir}/${roi_name}_${side}_${name}_weights.vtk --tract_data ${output_dir}/${roi_name}_${side}_${name}.tckdata
 done
 
-# if [ $SYSNAME == "hipergator" ]
-# then
-# 	module load python
-# fi
-
-# for side in left right
-# do
-#    python ${CODEDIR}/Python/MRtrix/tckConverter.py ${output_dir}/${roi_name}_${side}_${name}_ACPC.tck  ${output_dir}/${roi_name}_${side}_${name} --tract_data ${output_dir}/${roi_name}_${side}_${name}.csv
-#    python ${CODEDIR}/Python/MRtrix/Add_Weights_To_VTK.py --input_vtk ${output_dir}/${roi_name}_${side}_${name}_ACPC.vtk --output_vtk ${output_dir}/${roi_name}_${side}_${name}_weights.vtk --tract_data ${output_dir}/${roi_name}_${side}_${name}.tckdata
-# done
